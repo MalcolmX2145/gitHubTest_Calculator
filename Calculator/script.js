d4a2e4df63af6a9f9540b1b7a530448e017da411
MalcolmX2145 <130965426+MalcolmX2145@users.noreply.github.com>
2024-08-28 08:06:33 +0300

    Add files via upload

---
+++ Calculator/script.js
@@ -0,0 +1,95 @@
+let runningTotal = 0; // result of arithmetic
+let buffer = "0"; // default value on the screen div
+let previousOperator; // the operator that was put before the second operand
+
+const screen = document.querySelector(".screen");
+
+// function when button is pressed, display its value on screen div
+function buttonClick(value) {
+  if (isNaN(value)) {
+    handleSymbol(value);
+  } else {
+    handleNumber(value);
+  }
+  screen.innerText = buffer;
+}
+
+// function for operations of the symbols
+function handleSymbol(symbol) {
+  switch (symbol) {
+    case "C": // to clear
+      buffer = "0";
+      runningTotal = 0;
+      break;
+    case "=":
+      if (previousOperator === null) {
+        return;
+      }
+      flushOperation(parseInt(buffer)); // converts buffer to int to show it on the screen div
+      previousOperator = null; // removes previousOperator
+      buffer = runningTotal;
+      runningTotal = 0;
+      break;
+    case "←":
+      if (buffer.length === 1) {
+        buffer = "0";
+      } else {
+        buffer = buffer.substring(0, buffer.length - 1);
+      }
+      break;
+    case "+":
+    case "−":
+    case "×":
+    case "÷":
+      handleMath(symbol);
+      break;
+  }
+}
+
+// handles how the operators work with the operands
+function handleMath(symbol) {
+  if (buffer === "0") {
+    return;
+  }
+
+  const intBuffer = parseInt(buffer);
+
+  if (runningTotal === 0) {
+    runningTotal = intBuffer;
+  } else {
+    flushOperation(intBuffer);
+  }
+  previousOperator = symbol;
+  buffer = "0";
+}
+
+// operations for the arithmetic symbols
+function flushOperation(intBuffer) {
+  if (previousOperator === "+") {
+    runningTotal += intBuffer;
+  } else if (previousOperator === "−") {
+    runningTotal -= intBuffer;
+  } else if (previousOperator === "×") {
+    runningTotal *= intBuffer;
+  } else if (previousOperator === "÷") {
+    runningTotal /= intBuffer;
+  }
+}
+
+function handleNumber(numberString) {
+  if (buffer === "0") {
+    buffer = numberString;
+  } else {
+    buffer += numberString;
+  }
+}
+
+function init() {
+  document
+    .querySelector(".calc-buttons")
+    .addEventListener("click", function (e) {
+      buttonClick(e.target.innerText);
+    });
+}
+
+init();
